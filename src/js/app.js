3064535ebf2e5df1cdf170198f810cc4ca5752f9
ilyaChuk2004 <iliyachuk3004@gmail.com>
2021-02-04 15:38:24 +0300

    test1

---
+++ src/js/app.js
@@ -0,0 +1,35 @@
+import $ from 'dom7';
+import Framework7 from 'framework7/bundle';
+
+// Import F7 Styles
+import 'framework7/framework7-bundle.css';
+
+// Import Icons and App Custom Styles
+import '../css/icons.css';
+import '../css/app.less';
+
+
+// Import Routes
+import routes from './routes.js';
+// Import Store
+import store from './store.js';
+
+// Import main app component
+import App from '../app.f7.html';
+
+
+var app = new Framework7({
+  name: 'chuk', // App name
+  theme: 'auto', // Automatic theme detection
+  el: '#app', // App root element
+  component: App, // App main component
+
+  // App store
+  store: store,
+  // App routes
+  routes: routes,
+  // Register service worker
+  serviceWorker: {
+    path: '/service-worker.js',
+  },
+});
