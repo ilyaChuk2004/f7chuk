738ecd0cfa7212258e523dbaf63d0f155a4484e0
ilyaChuk2004 <iliyachuk3004@gmail.com>
2021-02-05 22:31:58 +0300

    вернул как было но без логики в app.js

--- src/js/app.js
+++ src/js/app.js
@@ -17,6 +17,32 @@ import store from './store.js';
 // Import main app component
 import App from '../app.f7.html';
 
+import mainscreen from '../comps/mainScreen/mainScreen.f7.html';
+import mainscreenItemDiv from '../comps/mainScreen/mainScreenItemDiv.f7.html';
+import mainscreenItemLink from '../comps/mainScreen/mainScreenItemLink.f7.html';
+import tabs from '../comps/tabs/tabs.f7.html';
+import tabsButt from '../comps/tabs/tabsButt.f7.html';
+import tabsButtItem from '../comps/tabs/tabsButtItem.f7.html';
+import about from '../comps/about.f7.html';
+import rhome from '../comps/rhome.f7.html';
+import png from '../comps/png.f7.html';
+
+function imp(tag, comp) {
+  Framework7.registerComponent(
+    'q-'+tag,
+    comp
+  )
+}
+
+imp('mainScreen', mainscreen);
+imp('mainScreenItemDiv', mainscreenItemDiv);
+imp('mainScreenItemLink', mainscreenItemLink);
+imp('tabs', tabs);
+imp('tabsButt', tabsButt);
+imp('tabsButtItem', tabsButtItem);
+imp('about', about);
+imp('rhome', rhome);
+imp('png', png);
 
 var app = new Framework7({
   name: 'chuk', // App name
