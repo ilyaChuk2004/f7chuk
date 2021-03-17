9b4ad02e791b0ed9810f9071b23da431c4e63930
ilyaChuk2004 <iliyachuk3004@gmail.com>
2021-03-17 12:59:56 +0300

    scroll event для вкладок больше не работает на десктопе

--- src/js/mods/events/s-scroll-event.js
+++ src/js/mods/events/s-scroll-event.js
@@ -1,15 +1,18 @@
 import $ from 'dom7';
 import {s_tabs} from '../s-tabs'
 import {s_ms_tips} from '../s-ms-tips'
+import store from '../../store';
 
 export function s_scroll_event(e) {
-
+  if (!store.state.appData.desktop) {
+    // debugger
     document.addEventListener('scroll', function (event) {
       console.log(event.target.className);
         if (event.target.className.includes('page-content') && event.target.className.includes('home')) { // or any other filtering condition        
           s_tabs('page-content');
         }
     }, true /*Capture event*/);
+  }
 
     $(document).on('click','.mainScreen-tip', (e)=>{
       s_ms_tips(e);
