abb5e9669e04f803a932b47884ddc32f2b4368f5
ilyaChuk2004 <iliyachuk3004@gmail.com>
2021-05-19 00:04:15 +0300

    •немного семантики •урл постов теперь без p •поддержка постов без галлереи и вебп превью •кампоненты вёрстки: параллакс и зачёркутый текст •лайки •новые меняющиеся иконки вкладок на главной

--- src/js/mods/s-ms-tips.js
+++ src/js/mods/s-ms-tips.js
@@ -4,6 +4,12 @@ export function s_ms_tips(e) {
     let tipId = tipEl.getAttribute('href').slice(-1);
 
     let tabEl = $(`.tabsB-item[href='#tab${tipId}']`);
+    for (let index = 0; index < tabEl[0].parentElement.children.length; index++) {
+        const element = tabEl[0].parentElement.children[index]
+        element.classList.remove('tab-link-active')
+        element.firstElementChild.innerText=element.firstElementChild.innerText.split('_')[0]
+    }
+    tabEl[0].firstElementChild.innerText+='_fill'
     tabEl.addClass('tab-link-active');
     
 }
